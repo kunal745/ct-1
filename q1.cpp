cd7fcd968aa8155ec499fc246b935819b68f2721
kunal403 <81363959+kunal745@users.noreply.github.com>
2022-08-26 13:43:18 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,38 @@
+//NAME - KUNAL
+//ROLLNO. - 2010990403
+//GROUP - G9
+//Q-1 REARRANGE AN ARRAY WITH ALTERNATE HIGH AND LOW ELEMENTS.
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter no. of array elements: ";  //declare the size of array
+    cin>>n;
+
+    int a[n];   //declare the array
+    
+	//input array elements
+    for(int i=0;i<n;i++)    
+    {
+        cin>>a[i];
+    }
+    
+    sort(a,a+n);    		//sorting the entered array 
+    
+    for(int i=0;i<n;i++)    //swap array elements so that middle element is greater than left and right elements.
+    {
+        if(i%2!=0 && i!=n-1)    //array elements whose index is odd and is not the last element of array will be swapped.
+        {
+            swap(a[i],a[i+1]);
+        }
+    }
+    
+    for(int i=0;i<n;i++)    //print final array
+    {
+        cout<<a[i]<<" ";
+    }
+    
+    return 0;
+}
---
+++ q2.cpp
@@ -0,0 +1,33 @@
+//NAME - KUNAL
+//ROLLNO - 2010990403
+//GROUP - G9
+//Q2 - FIND A PAIR WITH GIVEN SUM IN AN ARRAY
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,X,s=0;
+    cout<<"Enter no. of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter X: ";
+    cin>>X;
+    
+//we use two loops here because we will compare all elements (as the value of i changes) to all other elements(as value of j changes)
+//ex- array={1,2,3} and X=3 then we compare 1(i) with all elements then 2(i) with all elements and so and so as value of i and j changes.
+    for(int i=0;i<n;i++)    //we use two loops here for comparing elements
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if((arr[i]+arr[j])==X)
+            {
+                cout<<"("<<arr[i]<<" , "<<arr[j]<<")"<<endl;
+            }
+        }
+    }
+}
